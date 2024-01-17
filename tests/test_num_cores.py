14a4277401880895689452084578c191ce57426c
Marco Netto <marco.netto@gmail.com>
2024-01-17 15:27:24 -0800

    update num core testing

--- tests/test_num_cores.py
+++ tests/test_num_cores.py
@@ -1,31 +1,25 @@
+import os
 import sys
 
-from hpcadvisor import batch_handler, logger, main_cli, utils
+import pytest
 
-log = logger.logger
+from hpcadvisor import batch_handler
 
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: python test_num_cores.py <userinput> <sku>")
-        sys.exit(0)
 
-    userinput_file = sys.argv[1]
-    user_input = main_cli.get_userinput_from_file(userinput_file)
+def test_get_total_cores():
+    SUBSCRIPTION = os.environ.get("SUBSCRIPTION")
+    REGION = os.environ.get("REGION")
+    assert SUBSCRIPTION is not None
+    assert REGION is not None
 
-    rg_prefix = "test"
-    env_file = utils.generate_env_file(rg_prefix, user_input)
+    batch_handler.env["SUBSCRIPTION"] = batch_handler.get_subscription_id(SUBSCRIPTION)
 
-    batch_handler.env["SUBSCRIPTION"] = batch_handler.get_subscription_id(
-        user_input["subscription"]
-    )
-    batch_handler.env["REGION"] = user_input["region"]
+    batch_handler.env["REGION"] = REGION
 
     sku = "Standard_D2s_v3"
     cores = batch_handler._get_total_cores(sku)
-    print("sku: {}, cores: {}".format(sku, cores))
     assert cores == 2
 
     sku = "Standard_HB120-32rs_v2"
     cores = batch_handler._get_total_cores(sku)
-    print("sku: {}, cores: {}".format(sku, cores))
     assert cores == 32
