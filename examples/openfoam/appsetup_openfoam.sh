4dcdd6e197712974f2cc629adb7487b36f2d1cc7
Marco Netto <marco.netto@gmail.com>
2024-05-21 14:52:23 -0700

    remove gmx from script

--- examples/openfoam/appsetup_openfoam.sh
+++ examples/openfoam/appsetup_openfoam.sh
@@ -64,7 +64,6 @@ NP=\$((\$NODES*\$PPN))
 echo "NODES=\$NODES PPN=\$PPN"
 echo "hostprocmap=\$hostprocmap"
 
-APP_EXE=\$(which gmx_mpi)
 echo "Running OpenFOAM with \$NP processes ..."
 export UCX_NET_DEVICES=mlx5_ib0:1
 
