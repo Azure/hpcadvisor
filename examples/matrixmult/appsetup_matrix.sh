92a39647718605bf3a06ec844b2b3633978b732a
Marco Netto <marco.netto@gmail.com>
2024-07-18 09:47:50 -0700

    transition matrixmult example to new app script

--- examples/matrixmult/appsetup_matrix.sh
+++ examples/matrixmult/appsetup_matrix.sh
@@ -1,94 +1,42 @@
 #!/usr/bin/env bash
 
-APP_EXE=mpi_matrix_mult
-APP_CODE=mpi_matrix_mult.c
-APP_EXE_PATH="${AZ_BATCH_NODE_MOUNTS_DIR}/data/"
-
-set -x
-
-echo "APP_EXE_PATH=$APP_EXE_PATH"
-
-function setup_app_and_data {
-
-  source /etc/profile.d/modules.sh
-
-  module load mpi/openmpi
-
-  CODEURL=https://raw.githubusercontent.com/marconetto/testbed/main/mpi_matrix_mult.c
-
-  echo "APP_EXE_PATH=$MPI_EXE_PATH"
-  curl -sL $CODEURL -o "$APP_EXE_PATH"/"$APP_CODE"
-
-  echo "Compiling mpi code"
-  mpicc -o ${APP_EXE} ${APP_CODE}
-  ls -l ${APP_EXE}
-
-}
-
-function generate_run_script {
-
-  cat <<EOF >run_app.sh
-
-#!/bin/bash
-
-APP_EXE=${APP_EXE}
-APP_CODE=${APP_CODE}
-APP_EXE_PATH="\${AZ_BATCH_NODE_MOUNTS_DIR}/data/"
-cd \$APP_EXE_PATH
-APP_EXE_PATH=\${APP_EXE_PATH}/\${APP_EXE}
-
-
-
-APPINTERACTIONS=\${APPINTERACTIONS}
-APPMATRIXSIZE=\${APPMATRIXSIZE}
-[[ -z \$APPINTERACTIONS ]] && APPINTERACTIONS=10
-[[ -z \$APPMATRIXSIZE ]] && APPMATRIXSIZE=3000
+export APPEXEC=mpi_matrix_mult
 
 [[ -f /etc/bashrc ]] && . /etc/bashrc
 
 source /etc/profile.d/modules.sh
-
+module load gcc-9.2.0
 module load mpi/openmpi
 
-# Create host file
-batch_hosts=hosts.batch
-rm -rf \$batch_hosts
+hpcadvisor_setup() {
+  echo "main setup: $(pwd)"
 
-IFS=';' read -ra ADDR <<< "\$AZ_BATCH_NODE_LIST"
+  set -x
 
-[[ -z \$PPN ]] && echo "PPN not defined"
-PPN=\$PPN
+  CODEURL=https://raw.githubusercontent.com/marconetto/testbed/main/mpi_matrix_mult.c
+  APPCODE=$(basename $CODEURL)
+  curl -sL $CODEURL -o "$APPCODE"
 
-hostprocmap=""
+  mpicc -o "${APPEXEC}" "${APPCODE}"
 
-for host in "\${ADDR[@]}"; do
-    echo $host >> \$batch_hosts
-    hostprocmap="\$hostprocmap,\$host:\${PPN}"
-done
+  [[ $? -ne 0 ]] && return 1
 
-hostprocmap="\${hostprocmap:1}"
-
-NODES=\$(cat \$batch_hosts | wc -l)
-
-NP=\$((\$NODES*\$PPN))
-
-echo "NODES=\$NODES PPN=\$PPN"
-echo "hostprocmap=\$hostprocmap"
-set -x
-
-echo "=========VARIABLES======="
-set
-echo "========================="
-mpirun -np \$NP --oversubscribe --host \$hostprocmap --map-by ppr:\${PPN}:node \${APP_EXE_PATH} \${APPMATRIXSIZE} \${APPINTERACTIONS}
-
-EOF
-
-  chmod +x run_app.sh
-
+  return 0
 }
 
-[[ -f /etc/bashrc ]] && . /etc/bashrc
+hpcadvisor_run() {
+  echo "main run: $(pwd)"
 
-cd "$APP_EXE_PATH" || exit
-setup_app_and_data
-generate_run_script
+  ln -sf "../${APPEXEC}" .
+
+  [[ -z $APPINTERACTIONS ]] && APPINTERACTIONS=5
+  [[ -z $APPMATRIXSIZE ]] && APPMATRIXSIZE=1000
+
+  NP=$(($NODES * $PPN))
+  EXECPATH=$(realpath "${MPI_EXE}")
+
+  mpirun -np $NP --host "$AZ_HOST_LIST_PPN" --map-by ppr:"${PPN}":node "$EXECPATH" "${APPMATRIXSIZE}" "${APPINTERACTIONS}"
+  [[ $? -ne 0 ]] && return 1
+
+  return 0
+}
